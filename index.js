0f1312f2174d0840a197e791ffc30ea7c97439ad
jovan <jovancraft@gmail.com>
2024-01-22 11:27:02 -0600

    worked on learning about environment variables and plugging in args into the node terminal

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+//console.log(process.argv)//this shows an array consisting of the node path, the actual path, and whatever arguments you pass into the terminal. EX: node index.js foo bar baz
+//const foo = process.argv[2]
+//foo is number 3 in the array!
+//console.log(foo)
+
+//typing env into the terminal shows you a list on environment variables
+//console.log(process.env)//you would use this to get access to the environment variables
+//console.log(process.env.USER)//to get access to the USER variable inside the environment variables()says jovan which is me 😉
+
 const express = require('express');
 const server = express();
 
-server.use(express.json());
-
-console.log(process.argv)
-const foo = process.argv[2]
-console.log(foo)
+server.use(express.json());//teaches this server to read JSON from request bodies
 
 server.get('/hello', (req, res) => {
   res.json('hello, there');
 });
+
+
 
 server.use('*', (req, res) => {
   res.json({ message: 'API is UP!' })
