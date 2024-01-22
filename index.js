0b75f41552c1e702de9a68d6e0ee3b1c8c6977e6
jovan <jovancraft@gmail.com>
2024-01-22 11:59:35 -0600

    finished jotting down all the neccessary steps to deploy an app using heroku app and command line

--- index.js
+++ index.js
@@ -26,6 +26,29 @@ server.use('*', (req, res) => {
   const port = process.env.PORT || 9000
   //heroku requires you have a start script and a port that is grabbed from the heroku environment
 
+/*
+TO DEPLOY FROM HEROKU APP
+step 1: go to heroku.com and create a new account
+step 2: click the new button to create a new app
+step 3: in the deployment tab of the dashboard, you click on GitHub, make sure GitHub is configured to heroku, then when you see your GitHub username pop up, find the name of your project on GitHub
+step 4: click connect and it should say connected, can enable auto-deployment if you want to
+step 5: might be better to use manual deployment; scroll down and click deploy branch. you should then see a window opening
+step 6: heroku opened up a virtual machine, downloaded all of the resouces and dependencies needed and should give you a message telling you when it has been deployed successfully
+
+TO DEPLOY FROM HEROKU COMMAND LINE
+step 1: go to heroku.com and create a new account
+step 2: click the new button to create a new app
+step 3: this time in the deployment tab, hit heroku git button instead of Github button
+step 4: if youdownloaded the heroku cli correctly you should now be able to write heroku commands in the terminal
+step 5: can type 'heroku login' to get a pop up to let you log into the application, will tell you inside of the terminal when you are logged in correctly
+step 6: type 'heroku git:remote -a' followed by the heroku project name to connect it to your already set up git project
+step 7: type 'git push heroku main' to trigger deployment
+step 8: when this succeeds you will see a link pop up in the terminal you can click to go to the deployed app
+
+SIDENOTE
+  * can type in 'heroku logs -a' followed by the name of your project and ' --tail' to help with troubleshooting. It shows the activity going on in the deployed app
+
+*/
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
